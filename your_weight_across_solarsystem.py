7a980e8a479c75ec50f82e2858dd6b8d0b753336
LLTTF <137549204+LLTTF@users.noreply.github.com>
2024-08-23 20:43:43 +0530

    Add files via upload
    The following code calculates your weight on different planets of the solar system

---
+++ your_weight_across_solarsystem.py
@@ -0,0 +1,9 @@
+your_weight=int(input("Enter your weight in kg"))
+in_Mercury=your_weight*3.7
+in_Venus=your_weight*8.87
+in_Mars=your_weight*3.71
+in_Jupyter=your_weight*24.79
+in_Saturn=your_weight*10.44
+in_Uranus=your_weight*8.69
+in_Neptune=your_weight*11.15
+print(f"Your weight on Mercury would be {in_Mercury} kg\nYour weight on Venus would be {in_Venus} kg\nYour weight on Mars would be {in_Mars} kg\nYour weight on Jupyter would be {in_Jupyter} kg\nYour weight on Saturn would be {in_Saturn} kg\nYour weight on Uranus would be {in_Uranus} kg\nYour weight on Neptune would be {in_Neptune} kg\n")
